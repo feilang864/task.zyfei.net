848408b82056be14482622403367f7247348c649
yezi--change <2282636022@qq.com -- change>
2015-09-12 16:46:45 +0800

    [2485][PC][feature]: can show Profile part in /User/Summary

---
+++ SRV/ViewModel/User/SummaryModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FFLTask.SRV.ViewModel.User
+{
+    public class SummaryModel
+    {
+        public bool IsCurrentUser { get; set; }
+        public ProfileModel Profile { get; set; }
+    }
+}
